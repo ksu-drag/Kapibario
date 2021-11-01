519e33fe5a911c433b545219076410a2fcfe9249
ksu-drag <ksumastdrag@yandex.ru>
2021-11-01 12:13:12 +0300

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -6,7 +6,7 @@
 //    - and rotation
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
-const kapiPos = {x: canvas.width/2, y: canvas.height/2, speed: 5, size: 70, jump: 70};
+const kapiPos = {x: 100, y: canvas.height/2, speed: 5, size: 70, jump: 70};
 let leftPressed = false;
 let rightPressed = false;
 let jumpPressed = false;
@@ -14,12 +14,12 @@ let jumpCount = 0;
 let jumpLength = 50;
 let jumpHeight = 0;
 let kapibaraDirection;
-let x = canvas.width/2;
-let y = canvas.height-35;
+// let x = canvas.width/2;
+// let y = canvas.height-35;
 let playerHeight = kapiPos.size;
 let playerWidth = kapiPos.size;
 let paddleX = kapiPos.x;
-const game = {req: 0,req2: 0};
+const game = {req: 0};
 game.req = requestAnimationFrame(kapibaraDraw);
 
 	document.addEventListener("keydown", keyRightHandler, false);
@@ -49,22 +49,22 @@ game.req = requestAnimationFrame(kapibaraDraw);
 		}
 	}
 function kapibaraDraw (){
-    // movementPlayer();
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+   ctx.clearRect(0, 0, canvas.width, canvas.height);
+    earth();
 /////////////////////// BODY 
 ctx.beginPath();
     ctx.fillStyle = 'Sienna';
-    ctx.ellipse(paddleX, canvas.height-playerHeight-jumpHeight, 30, 50, 80, 0, Math.PI*2);
+    ctx.ellipse(paddleX, canvas.height/2-playerHeight-jumpHeight, 30, 50, 80, 0, Math.PI*2);
     ctx.fill();
 
  /////////////////////// HEAD
 ctx.beginPath();
 if( !kapibaraDirection){
-      ctx.ellipse(paddleX - 48, canvas.height-playerHeight-jumpHeight-50, 20, 30, 80, 0, Math.PI*2);
-      ctx.rect(paddleX-48, canvas.height-playerHeight-jumpHeight - 50, 30, 45);
+      ctx.ellipse(paddleX - 48, canvas.height/2-playerHeight-jumpHeight-50, 20, 30, 80, 0, Math.PI*2);
+      ctx.rect(paddleX-48, canvas.height/2-playerHeight-jumpHeight - 50, 30, 45);
 }else if(kapibaraDirection){
-      ctx.ellipse(paddleX +48, canvas.height-playerHeight-jumpHeight-50, 20, 30, 80, 0, Math.PI*2);
-      ctx.rect(paddleX +20, canvas.height-playerHeight-jumpHeight - 50, 30, 45);
+      ctx.ellipse(paddleX +48, canvas.height/2-playerHeight-jumpHeight-50, 20, 30, 80, 0, Math.PI*2);
+      ctx.rect(paddleX +20, canvas.height/2-playerHeight-jumpHeight - 50, 30, 45);
 }
     ctx.fill();
 //////////////////// TAIL
@@ -73,46 +73,47 @@ ctx.beginPath();
     ctx.lineWidth = 10;
     ctx.lineCap = "round";
 if(!kapibaraDirection){
-  ctx.moveTo(paddleX +30, canvas.height-playerHeight-jumpHeight);
-  ctx.lineTo(paddleX +60, canvas.height-playerHeight-jumpHeight -20);
+  ctx.moveTo(paddleX +30, canvas.height/2-playerHeight-jumpHeight);
+  ctx.lineTo(paddleX +60, canvas.height/2-playerHeight-jumpHeight -20);
 }else if(kapibaraDirection){
-  ctx.moveTo(paddleX -30, canvas.height-playerHeight-jumpHeight);
-  ctx.lineTo(paddleX -60, canvas.height-playerHeight-jumpHeight -20);
+  ctx.moveTo(paddleX -30, canvas.height/2-playerHeight-jumpHeight);
+  ctx.lineTo(paddleX -60, canvas.height/2-playerHeight-jumpHeight -20);
 }
      ctx.stroke();
 /////////////////// EYES
 ctx.beginPath();
 ctx.fillStyle = 'black';
  if( !kapibaraDirection ){
-   ctx.arc(paddleX-50, canvas.height-playerHeight-jumpHeight -50, 5,0, Math.PI*2)
+   ctx.arc(paddleX-50, canvas.height/2-playerHeight-jumpHeight -50, 5,0, Math.PI*2)
  }else if(kapibaraDirection){
-   ctx.arc(paddleX +50, canvas.height-playerHeight-jumpHeight -50, 5,0, Math.PI*2)
+   ctx.arc(paddleX +50, canvas.height/2-playerHeight-jumpHeight -50, 5,0, Math.PI*2)
  }
    ctx.fill();
 ///////////////////  EARS         
 ctx.beginPath();
     ctx.fillStyle = 'Sienna';
 if (!kapibaraDirection){
-    ctx.ellipse(paddleX -30, canvas.height-playerHeight-jumpHeight - 70, 7, 4, 80, 0, Math.PI*2);
-    ctx.ellipse(paddleX -40, canvas.height-playerHeight-jumpHeight - 73, 7, 4, 80, 0, Math.PI*2);
+    ctx.ellipse(paddleX -30, canvas.height/2-playerHeight-jumpHeight - 70, 7, 4, 80, 0, Math.PI*2);
+    ctx.ellipse(paddleX -40, canvas.height/2-playerHeight-jumpHeight - 73, 7, 4, 80, 0, Math.PI*2);
 }else if(kapibaraDirection){
-    ctx.ellipse(paddleX +30, canvas.height-playerHeight-jumpHeight - 70, 7, 4, 80, 0, Math.PI*2);
-    ctx.ellipse(paddleX +40, canvas.height-playerHeight-jumpHeight- 73, 7, 4, 80, 0, Math.PI*2);
+    ctx.ellipse(paddleX +30, canvas.height/2-playerHeight-jumpHeight - 70, 7, 4, 80, 0, Math.PI*2);
+    ctx.ellipse(paddleX +40, canvas.height/2-playerHeight-jumpHeight- 73, 7, 4, 80, 0, Math.PI*2);
 }
     ctx.fill();
 ////////////////////// FEET
     ctx.beginPath();
     ctx.lineWidth = 14;
-    ctx.moveTo(paddleX -30, canvas.height-playerHeight-jumpHeight +10);
-    ctx.lineTo(paddleX -40, canvas.height-playerHeight-jumpHeight + 50);
-    ctx.moveTo(paddleX+20, canvas.height-playerHeight-jumpHeight +10);
-    ctx.lineTo(paddleX +30, canvas.height-playerHeight-jumpHeight +50);
+    ctx.moveTo(paddleX -30, canvas.height/2-playerHeight-jumpHeight +10);
+    ctx.lineTo(paddleX -40, canvas.height/2-playerHeight-jumpHeight + 50);
+    ctx.moveTo(paddleX+20, canvas.height/2-playerHeight-jumpHeight +10);
+    ctx.lineTo(paddleX +30, canvas.height/2-playerHeight-jumpHeight +50);
     ctx.stroke();    
 ////////////////////
 game.req = requestAnimationFrame(kapibaraDraw);
 };
 function draw(){
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
+    earth();
 	if(rightPressed && paddleX < canvas.width-playerWidth){
 		paddleX += 7;
 	}
@@ -128,8 +129,22 @@ function draw(){
     jumpPressed=false;
     jumpHeight=0;
 	}
-  
+    if(paddleX > 300){
+    canvas.height/2-playerHeight-jumpHeight/2;
+   }
 	kapibaraDraw();
-
+    kapiEarth ();
 }
 setInterval(draw, 10);
+
+function kapiEarth () {
+   
+}
+
+
+
+function earth(){
+    ctx.fillStyle = "green";
+    ctx.fillRect(0, 400, 300, 500);
+    ctx.fillRect(300, 600, 300, 300);
+}
